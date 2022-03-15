8e6764292c6fdd11b0f948091da9192982056684
Glicio <glicioo@outlook.com>
2022-03-15 20:32:38 -0300

    add images

--- src/components/Cnds.jsx
+++ src/components/Cnds.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from 'react'
 
+import {ReactComponent as Caixa} from '../static/imgs/caixa.svg'
+import {ReactComponent as Arapiraca} from '../static/imgs/arapiraca.svg'
+
+import estadual from '../static/imgs/brasao_alagoas-463630d216.png'
+import maceio from '../static/imgs/maceio.png'
+import tst from '../static/imgs/tst.png'
+import receita from '../static/imgs/receita_federal.png'
+
+
 
 
 const PJ = (props) => {
@@ -7,19 +16,30 @@ const PJ = (props) => {
         <>
             <div className="links" id='pj' style={{display: props.display}}>
                 <h3 className="titulo3">Pessoa Jurídica</h3>
-                <a href="http://contribuinte.sefaz.al.gov.br/certidao/" className="btn" target='_blank' rel='noreferrer'>Estadual</a>
-                <a href="https://consulta-crf.caixa.gov.br/consultacrf/pages/consultaEmpregador.jsf" className="btn" target='_blank' rel='noreferrer'>FGTS</a>
-                <a href="https://cndt-certidao.tst.jus.br/inicio.faces" className="btn" target='_blank' rel='noreferrer'>Trabalhista</a>
-                <a href="https://semecmaceio.com/?opcao=certidoes" className="btn" target='_blank' rel='noreferrer'>Maceió</a>
-                <a href="https://arapiraca.abaco.com.br/eagata/portal/" className="btn" target='_blank' rel='noreferrer'>Arapiraca</a>
-                <div>
+                <a href="http://contribuinte.sefaz.al.gov.br/certidao/" className="btn" target='_blank' rel='noreferrer'>
+                    <img src={estadual} alt="" className="logo" />Estadual: Alagoas
+                </a>
+                <a href="https://consulta-crf.caixa.gov.br/consultacrf/pages/consultaEmpregador.jsf" className="btn" target='_blank' rel='noreferrer'>
+                    <Caixa className="logo"/>FGTS
+                </a>
+                <a href="https://cndt-certidao.tst.jus.br/inicio.faces" className="btn" target='_blank' rel='noreferrer'>
+                    <img src={tst} alt="" className="logo" />Trabalhista
+                </a>
+                <a href="https://semecmaceio.com/?opcao=certidoes" className="btn" target='_blank' rel='noreferrer'>
+                    <img src={maceio} alt="" className="logo" />Maceió
+                </a>
+                <a href="https://arapiraca.abaco.com.br/eagata/portal/" className="btn" target='_blank' rel='noreferrer'>
+                    <Arapiraca className='logo'/>Arapiraca
+                </a>
+                <div className=''>
                     <h3 className='titulo3'>Receita Federal</h3>
+                    
                     <div className='sub-menu'>
-                        <a href="https://servicos.receita.fazenda.gov.br/Servicos/certidaointernet/PJ/Emitir" className="btn" target='_blank' rel='noreferrer'>Emitir Nova</a>
-                        <a href="https://servicos.receita.fazenda.gov.br/Servicos/CertidaoInternet/PJ/Consultar" className="btn" target='_blank' rel='noreferrer'>Segunda Via</a>
+                        <a href="https://servicos.receita.fazenda.gov.br/Servicos/certidaointernet/PJ/Emitir" className="btn" target='_blank' rel='noreferrer'><img src={receita} alt="" className="logo" />Emitir Nova</a>
+                        <a href="https://servicos.receita.fazenda.gov.br/Servicos/CertidaoInternet/PJ/Consultar" className="btn" target='_blank' rel='noreferrer'><img src={receita} alt="" className="logo" />Segunda Via</a>
                     </div>
                     <div className='sub-menu'>
-                        <a href="http://servicos.receita.fazenda.gov.br/Servicos/cnpjreva/cnpjreva_solicitacao.asp" className="btn" target='_blank' rel='noreferrer'>Situação Cadastral</a>
+                        <a href="http://servicos.receita.fazenda.gov.br/Servicos/cnpjreva/cnpjreva_solicitacao.asp" className="btn" target='_blank' rel='noreferrer'><img src={receita} alt="" className="logo" />Situação Cadastral</a>
                     </div>
                 </div>
             </div>
@@ -32,9 +52,15 @@ const PF = (props) => {
         <>
             <div className="links" id='pf' style={{display: props.display}}>
                 <h3 className="titulo3">Pessoa Física</h3>
-                <a href="https://contribuinte.sefaz.al.gov.br/certidao/#/" className="btn" target='_blank' rel='noreferrer'>Estadual: Alagoas</a>
-                <a href="https://cndt-certidao.tst.jus.br/inicio.faces" className="btn" target='_blank' rel='noreferrer'>Trabalhista</a>
-                <a href="http://solucoes.receita.fazenda.gov.br/Servicos/certidaointernet/PF/Emitir" className="btn" target='_blank' rel='noreferrer'>Federal</a>
+                <a href="https://contribuinte.sefaz.al.gov.br/certidao/#/" className="btn" target='_blank' rel='noreferrer'>
+                    <img src={estadual} alt="" className="logo" />Estadual: Alagoas
+                </a>
+                <a href="https://cndt-certidao.tst.jus.br/inicio.faces" className="btn" target='_blank' rel='noreferrer'>
+                    <img src={tst} alt="" className="logo" />Trabalhista
+                </a>
+                <a href="http://solucoes.receita.fazenda.gov.br/Servicos/certidaointernet/PF/Emitir" className="btn" target='_blank' rel='noreferrer'>
+                    <img src={receita} alt="" className="logo" />Federal
+                </a>
             </div>
         </>
     )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './NavBar.css'
 import { Link } from 'react-router-dom'
+import {ReactComponent as Certidao} from '../static/imgs/certidao.svg'
+import {ReactComponent as Folha} from '../static/imgs/folha.svg'
 
 
 export default function NavBar(props){
     return(
         <div className='nav-bar'>
             <span>Links úteis e classificação de folhas</span>
-            <div className="buttons-div">
-                <Link to='/'>Certidões</Link>
-                <Link to='/folha'>Folha</Link>
+            <div className="btn-div">
+                <Link to='/' className='btn'><Certidao className='icon'/> Certidões</Link>
+                <Link to='/folha' className='btn'><Folha className='icon'/>Folha</Link>
             </div>
         </div>
     )
