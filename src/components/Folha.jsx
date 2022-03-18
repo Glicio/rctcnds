d5f31dff6b8b4d5661eef2356ad60ffa694257c4
Glicio <glicioo@outlook.com>
2022-03-17 23:20:31 -0300

    learning react-tables

--- src/components/Folha.jsx
+++ src/components/Folha.jsx
@@ -1,18 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
+import { useTable } from 'react-table/dist/react-table.development'
 
 
 
-let data = [
-    {tipo: "01", valor: 399.10},
-    {tipo: "10", valor: 962.70},
-    {tipo: "01", valor: 1000.00},
-    {tipo: "33", valor: 777.18},
+const data = [  
+    {
+        tipo: "01", valor: 399.10
+    },
+    {
+        tipo: "10", valor: 962.70
+    },
+    {
+        tipo: "01", valor: 1000.00
+    },
+    {
+        tipo: "33", valor: 777.18
+    },
 ]
 
 
 
 export default function Folha(props){
+
+    const columns = useMemo(() => [
+        {
+            Header: 'Tipo',
+            accessor: 'tipo',
+        },
+        {
+            Header: 'Valor',
+            accessor: 'valor',
+        },
+        {
+            Header:'Opções',
+            accessor:'opt'
+        },
+    ],[])
+
+    const editableCell= ({value: valorInicial, row: index, column: id}) => {
+        return <input value={valorInicial}/>;
+    }
+
     const [tableData, setTableData] = useState(data)
+    const tableInstance = useTable({columns,data: tableData, defaultColumn: {Cell: editableCell}})
+    const {
+        headers,
+        getTableProps,
+        getTableBodyProps,
+        rows,
+        prepareRow,
+      } = tableInstance
     
     function remover(index){    
         let result = []
@@ -24,24 +61,33 @@ export default function Folha(props){
         setTableData(result)
     }
 
+    
+
+
     return (
     <>
         <h1>Folha</h1>
-        <table>
+        <table {...getTableProps()}>
             <thead>
                 <tr>
-                    <th>TIPO</th>
-                    <th>VALOR</th>
+                {headers.map((column => {
+                        return <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                    }))}
                 </tr>
             </thead>
-            <tbody>
-                {tableData.map((item, index) => { return (
-                    <tr key={index}>
-                        <td>{item.tipo}</td>
-                        <td>{item.valor}</td>
-                        <td><button onClick={() => remover(index)}>Remover</button></td>
-                    </tr>
-                )})}
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map((cell) => {
+                                
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                            <td key={row.index}><button onClick={() => {remover(row.index)}}>Remover</button></td>
+                        </tr>
+                    )
+                })}
             </tbody>
         </table>
     </>)
